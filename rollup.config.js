a91d0e41964111d42c91d7e91e5f73fa0144b162
MeetinaXD <capitaycity@gmail.com>
2023-03-03 17:18:12 +0800

    chore: update build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,49 @@
 const typescript = require('@rollup/plugin-typescript')
+const nodeResolve = require('@rollup/plugin-node-resolve')
+const terser = require('@rollup/plugin-terser')
 
 const pkg = require('./package.json')
+
+function minimize(list) {
+  const ret = list.map(e => ({
+    ...e,
+    file: e.file.replace('.js', '.min.js'),
+    sourcemap: false,
+    plugins: [
+      terser()
+    ]
+  }))
+
+  return ret
+}
+
+const outputs = [
+  {
+    file: pkg.exports['.'].require,
+    format: 'cjs',
+    sourcemap: true,
+  },
+  {
+    file: pkg.exports['.'].import,
+    format: 'esm',
+    sourcemap: true,
+  },
+  {
+    file: pkg.exports['.'].browser,
+    format: 'iife',
+    sourcemap: true,
+    name: 'SSEClient',
+  },
+]
 
 module.exports = {
   input: './index.ts',
   output: [
-    {
-      file: pkg.exports['.'].require,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: pkg.exports['.'].import,
-      format: 'esm',
-      sourcemap: true,
-    },
-    {
-      file: pkg.exports['.'].browser,
-      format: 'iife',
-      sourcemap: true,
-      name: 'SSEClient'
-    },
+    ...outputs,
+    ...minimize(outputs)
   ],
   plugins: [
-    typescript()
+    typescript(),
+    nodeResolve()
   ],
 }
